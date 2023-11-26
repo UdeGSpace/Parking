52a4134db75dd31430f7afae76d23dd1223cd341
UDEG-Space <85599872+UdeGSpace@users.noreply.github.com>
2023-11-26 17:00:28 -0600

    remove url

--- API/index.js
+++ API/index.js
@@ -29,7 +29,6 @@ database.once('connected', async () => {
 });
 
 const app = express();
-app = app.create({baseUrl:'https://parking-api-1b5j.onrender.com'})
 app.use(express.json());
 app.use(cors());
 
