4360af4fbf8cb33d4f9a83a8371379c5fb8e5c78
UDEG-Space <85599872+UdeGSpace@users.noreply.github.com>
2023-11-26 04:19:49 -0600

    Test deploy

--- API/index.js
+++ API/index.js
@@ -4,31 +4,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 const mongoString = process.env.MONGO_DB_URL;
 
-const database = async () => {
-    try {
-        mongoose.set('strictQuery', false);
-        await mongoose.connect(mongoString, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB Connected...');
-    } catch (err) {
-        console.error(err.message);
-        // make the process fail
-        process.exit(1);
-    }
-}
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+const collection = database.collection("EntraceRegister");
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen()
-console.log(`Server Started`)
+
+app.listen(() => {
+    console.log(`Server Started at ${3000}`)
+})
 
 const routes = require('./routes/routes');
 app.use('/parking', routes)
 
 module.exports = {
     database,
+    collection
 };
